b13f80d3875d7b0fe16656a305c9df4553c0d08e
Sam Bozek <sambozek@gmail.com>
2022-09-01 11:43:46 -0700

    Basic app complete

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from distutils.log import debug
-import dash
-from dash.dependencies import Input, Output, State
-from dash import Dash, dcc, html, dash_table
+from dash.dependencies import Input, Output
+from dash import Dash, dcc, html
 import dash_bootstrap_components as dbc
 import pandas as pd
 
-app = Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = html.Div([
+    html.H1("Sam's Reverse Compositer for FASTA Files"),
+
+    html.Div("""Copy And Paste Single FASTA Result for the Reverse Composite. 
+    Currently assuming only ACTG are in the results"""),
 
     dcc.Textarea(
         id='fasta-DNA-input',
@@ -35,6 +37,7 @@ TTAAGAGTTGAGTCTCCCCCATTTAA
 
 ])
 
+
 def fasta_parse(fasta_text):
     """Parse the input FASTA data
 
@@ -47,6 +50,7 @@ def fasta_parse(fasta_text):
     fasta_df = pd.DataFrame(fasta_split, columns=['Name', 'DNA_String'])
     return fasta_df
 
+
 def dna_check(dna_str):
     """Check to make sure that there are no characters outside of the 
     Args:
@@ -56,6 +60,19 @@ def dna_check(dna_str):
     """
     nucleotides = set(['A', 'C', 'G', 'T', 'a', 'c', 'g', 't'])
     return set(dna_str.replace('\n', '')) <= nucleotides
+
+
+def dna_reverse_complement(dna_str):
+    """Generates the reverse complement of a dna string passed to it.
+    Args:
+        dna_str (str): A dna string
+    Returns:
+        str: the reverse complement of the input dna_str
+    """
+    complement = dict(zip("ACGTacgt\n", "TGCAtgca\n"))
+    reverse_complement = ''.join([complement[base]
+                                 for base in dna_str[::-1]])
+    return reverse_complement
 
 
 @app.callback(
@@ -72,12 +89,15 @@ def input_name(value):
 )
 def reverse_complement(value):
     fasta_df = fasta_parse(value)
-    if dna_check(fasta_df['DNA_String'][0]):
-        return fasta_df['DNA_String'][0]
+    dna_string = fasta_df['DNA_String'][0]
+    if dna_check(dna_string):
+        reverse_complement = dna_reverse_complement(dna_string).replace('\n', '')
+        reverse_complement = '\n'.join([reverse_complement[i: i+60] for i in range(0, len(reverse_complement), 60)])
+        return reverse_complement
 
     else:
         return "Not Valid DNA String"
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False)
