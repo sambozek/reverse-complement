c290a947f040115cfe7fbbc3bc15319def4c6317
Sam Bozek <sambozek@gmail.com>
2022-09-01 10:40:50 -0700

    Can Parse Fasta File for Name / DNA String. Next is to check DNA string to make sure there are no non DNA characters present

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from dash import Dash, dcc, html, dash_table
 import dash_bootstrap_components as dbc
 import pandas as pd
 
-app = Dash(__name__)
+app = Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = html.Div([
 
@@ -29,10 +29,30 @@ TTAAGAGTTGAGTCTCCCCCATTTAA
         """,
         style={'width': '75%', 'height': 300},
     ),
+    html.H2(id='dna-label', style={'whiteSpace': 'pre-line', 'wrap': True}),
     html.Div(id='reverse-complement',
              style={'whiteSpace': 'pre-line', 'wrap': True})
 
 ])
+
+def fasta_parse(fasta_text):
+    """Parse the input FASTA data
+
+    Args:
+        fasta_text (str): Multiline file with name and info denoted by line containing '>' and followed by DNA Data afterwards
+    Returns:
+        pd.DataFrame: A dataframe containing the name of the data and the associated DNA string
+    """    
+    fasta_split = [fasta.split('\n',1) for fasta in fasta_text.strip().split('>')[1:]]
+    fasta_df = pd.DataFrame(fasta_split, columns=['Name', 'DNA_String'])
+    return fasta_df
+
+@app.callback(
+    Output('dna-label', 'children'),
+    Input('fasta-DNA-input', 'value')
+)
+def input_name(value):
+    return "Reverse Composite for {}:".format(fasta_parse(value)['Name'][0])
 
 
 @app.callback(
@@ -40,7 +60,7 @@ TTAAGAGTTGAGTCTCCCCCATTTAA
     Input('fasta-DNA-input', 'value')
 )
 def reverse_complement(value):
-    return value
+    return fasta_parse(value)['DNA_String']
 
 
 if __name__ == '__main__':
