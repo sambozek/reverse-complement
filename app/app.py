81683200de4b6191cfbe8a19554096da8813cb78
Sam Bozek <sambozek@gmail.com>
2022-09-01 23:02:54 -0700

    Check to make sure FASTA format is followed (to my understanding). Finalize docker file and double/triple check. Some text to clarify inputs.

--- app/app.py
+++ app/app.py
@@ -7,14 +7,16 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
 app.layout = html.Div([
-    html.H1("Sam's Reverse Compositer for FASTA Files"),
+    html.H1("Sam's Reverse Compositer for FASTA Files", style={'textAlign': 'center'}),
 
-    html.Div("""Copy And Paste Single FASTA Result for the Reverse Composite. 
-    Currently assuming only ACTG are in the results"""),
+    html.Div("Copy And Paste Single FASTA Result for the Reverse Composite."),
+    html.Div("Currently assuming only ACTG are in the DNA string"),
+    html.Div("The output is updated live as the input changes provided that the input follows the correct format"),
+    html.Div("Example of expected format is in the text area below:"),
 
     dcc.Textarea(
         id='fasta-DNA-input',
-        value=""">Test1 
+        value=""">Test1
 ACCAGTGCCGGACTTAACCGCGTTCACAACAGCCTTGTCCGCTTATGCCCAGGAGACAGT
 AGTGATTGGCACCAAAGGGAAAGTCTAGAATTACCGCTAAGTCGTCCGAAGTCCGGGCAC
 GATCCTCTACTAGTATACGCACCCTGCGGTAATACTAGGAAACTAAGGTGGTTTACACCG
@@ -30,7 +32,7 @@ TTCTCGCCTACGAATTGTTGGAACGTCCAAGTACTTCTATAAGAGAAGCTCTTTCTGGCT
 GCAAACGTCTCCGTAACCGGTACTCGAACGCTTACCAGCCGCCATCCGTCTTCCATTGGG
 TTAAGAGTTGAGTCTCCCCCATTTAA
         """,
-        style={'width': '75%', 'height': 300},
+        style={'width': '50%', 'height': 300, 'wrap': "hard"},
     ),
     html.H2(id='dna-label', style={'whiteSpace': 'pre-line', 'wrap': True}),
     html.Div(id='reverse-complement',
@@ -40,16 +42,19 @@ TTAAGAGTTGAGTCTCCCCCATTTAA
 
 
 def fasta_parse(fasta_text):
-    """Parse the input FASTA data
+    """Parse the input FASTA data, if missing the 
 
     Args:
         fasta_text (str): Multiline file with name and info denoted by line containing '>' and followed by DNA Data afterwards
     Returns:
         pd.DataFrame: A dataframe containing the name of the data and the associated DNA string
-    """    
-    fasta_split = [fasta.split('\n',1) for fasta in fasta_text.strip().split('>')[1:]]
-    fasta_df = pd.DataFrame(fasta_split, columns=['Name', 'DNA_String'])
-    return fasta_df
+    """
+    if fasta_text.strip()[0] == '>':
+        fasta_split = [fasta.split('\n', 1) for fasta in fasta_text.strip().split('>')[1:]]
+        fasta_df = pd.DataFrame(fasta_split, columns=['Name', 'DNA_String'])
+        return fasta_df
+    else:
+        return pd.DataFrame({'Name': ["Invalid"], 'DNA_String': ["Improperly Formatted FASTA input"]})
 
 
 def dna_check(dna_str):
@@ -81,7 +86,7 @@ def dna_reverse_complement(dna_str):
     Input('fasta-DNA-input', 'value')
 )
 def input_name(value):
-    return "Reverse Composite for {}:".format(fasta_parse(value)['Name'][0])
+    return ">{} Reverse Composite:".format(fasta_parse(value)['Name'][0])
 
 
 @app.callback(
@@ -95,10 +100,8 @@ def reverse_complement(value):
         reverse_complement = dna_reverse_complement(dna_string).replace('\n', '')
         reverse_complement = '\n'.join([reverse_complement[i: i+60] for i in range(0, len(reverse_complement), 60)])
         return reverse_complement
+    elif dna_string == 'Improperly Formatted FASTA input':
+        return dna_string
 
     else:
-        return "Not Valid DNA String"
-
-
-# if __name__ == '__main__':
-#     app.run_server(host='0.0.0.0', port=1234)
+        return "Invalid DNA String"
